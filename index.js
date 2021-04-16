2f6c86e00a5c7c47f07a715d5b5fc31541daf2ba
arkulnid <arkulnid@gmail.com>
2021-04-17 00:02:55 +0500

    United all classes in single

--- index.js
+++ index.js
@@ -1,294 +1,109 @@
-const tetromino = [[	
-	[[1,0],
-	 [1,1],
-	 [1,0]],
-	[[1,1,1],
-	 [0,1,0]],
-	[[0,1],
-	 [1,1],
-	 [0,1]],
-	[[0,1,0],
-	 [1,1,1]]
-],[
-	[[1,1],
-	 [1,1]],
-	[[1,1],
-	 [1,1]],
-	[[1,1],
-	 [1,1]],
-	[[1,1],
-	 [1,1]]
-],[
-	[[1,0,0,0],
-	 [1,0,0,0],
-	 [1,0,0,0],
-	 [1,0,0,0]],
-	[[1,1,1,1]],
-	 [[1],
-	 	[1],
-	 	[1],
-	 	[1]],
-	[[1,1,1,1]]
-],[
-	[[1,0], 
-   [1,0], 
-   [1,1]],
-	[[1,1,1],
-	 [1,0,0]],
-	[[1,1],
-	 [0,1],
-	 [0,1]],
-	[[0,0,1],
-	 [1,1,1]]
-]]
-
-const boardMap = [    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],     
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],     
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    
-  [0, 0, 0, 0, 0, 0, 1, 0, 1, 1]    
-]
-
-class V2 {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	add(v) {
-		return new V2(this.x + v.x, this.y + v.y);
-	}
-	
-	scale(s) {
-		return new V2(this.x * s, this.y * s);
-	}
-}
-
-const directionMap = {
-  'ArrowDown': new V2(0, 1.0),
-  'ArrowRight': new V2(1.0, 0),
-  'ArrowLeft': new V2(-1.0, 0)
-}
-
-class Board {
+class Tetris {
 	constructor(context) {
 		this.context = context;
-	}
+		this.frameCounter = 0;
+		this.gameLoop = false;
+		this.piecePosition = [0, 0];
 
-	fillBoard() {
-		for (let i = 0; i < boardMap.length; i++) {
-			for (let j = 0; j < boardMap[i].length; j++) {
-				if (boardMap[i][j] == 1) {
-					this.context.fillStyle = 'red';
-				} else { 
-					this.context.fillStyle = 'black';
-				}
-				this.context.fillRect(20 * j, 20 * i, 19, 19);
+		this.boardWidth = BOARD_WIDTH;
+		this.boardWeight = BOARD_HEIGHT;
+
+		this.squareSide = 30; // square size
+
+		this.pieces = [
+			{
+				id: 0,
+				name: 'z',
+				color: 'red',
+				rotation: Z_ROT
+			},
+			{
+				id: 1,
+				name: 's',
+				color: 'green',
+				rotation: S_ROT
+			},
+			{
+				id: 2,
+				name: 'o',
+				color: 'blue',
+				rotation: O_ROT
+			},
+			{
+				id: 3,
+				name: 'l',
+				color: 'orange',
+				rotation: L_ROT
+			},
+			{
+				id: 4,
+				name: 'j',
+				color: 'purple',
+				rotation: J_ROT
+			},
+			{
+				id: 5,
+				name: 't',
+				color: 'yellow',
+				rotation: T_ROT
+			},
+			{
+				id: 6,
+				name: 'i',
+				color: 'brown',
+				rotation: I_ROT
 			}
-		}
-	}
+		];
 
-	update() {
-		this.fillBoard()
-	}
-}
+		this.piece = this.pieces[0]; 	// current piece
+		this.pieceRotation = 0; 			// current piece's position
+		this.next = this.pieces[0];   // next piece
 
-class Piece {
-	constructor(pos, context) {
-		this.context = context;
-		this.tetromino = tetromino;
-		this.pos = pos;
-		this.potentialPos = new V2(0, 0);
-		this.pieceIndex = Math.floor(Math.random() * (4 - 0) + 0);
-		this.pieceShape = 0;
-	}
-		
-	new() {
-		this.pos = new V2(0,0);
-		this.potentialPos = new V2(0,0);
-		this.pieceIndex = Math.floor(Math.random() * (4 - 0) + 0);
-		this.pieceShape = 0;
-		this.fillPiece();
-	}
 
-	fillPiece() {
-		this.context.fillStyle = "red";
-		for (let i = 0; i < tetromino[this.pieceIndex][this.pieceShape].length; i++) {
-			for (let j = 0; j < tetromino[this.pieceIndex][this.pieceShape][i].length; j++) {
-				if (tetromino[this.pieceIndex][this.pieceShape][i][j] == 1) {
-					this.context.fillRect(20 * j + this.pos.x, 20 * i + this.pos.y, 19, 19);
-				}
-			}
-		}
-	}
-
-	getWidth() {
-		return 20 * Math.max(...tetromino[this.pieceIndex][this.pieceShape].map((x) => {
-				let i = 0, acc = 0;
-				for (; i < x.length; i++) {
-					if (x[i] > 0) { ++acc; }
-				} 
-				return acc;
-			}))
 	}
 	
-	getHeight() {
-		return 20 * tetromino[this.pieceIndex][this.pieceShape].length;
-	}
 
-	canSetPos(pos) {
-		let row = (pos.y / 20);
-		let col = (pos.x / 20);
-		console.log(pos.y)
-		let tetRow = (this.getHeight() / 20);
-		let tetCol = (this.getWidth() / 20);
-		if (row + (this.getHeight() / 20) - 1 >= boardMap.length
-				&& col + (this.getWidth() / 20) - 1 <= boardMap[0].length) {
-			return false;
-		}
-		for (let i = 0; i < tetRow; i++) {
-			for (let j = 0; j < tetCol; j++) {
-				if (boardMap[row + i][col + j] > 0 &&
-						tetromino[this.pieceIndex][this.pieceShape][i][j] > 0) {
-
-					return false;
-				}
-			}
-		}
-
-		return true
-	}
-
-	freeze() {
-		let row = (this.pos.y / 20);
-		let col = (this.pos.x / 20);
-		let tetRow = (this.getHeight() / 20);
-		let tetCol = (this.getWidth() / 20);
-
-		for (let i = 0; i < tetRow; i++) {
-			for (let j = 0; j < tetCol; j++) {
-				if (tetromino[this.pieceIndex][this.pieceShape][i][j] > 0) {
-					boardMap[row + i][col + j] = tetromino[this.pieceIndex][this.pieceShape][i][j];
-					console.log(boardMap)
-				}
-			}
-		}
+	async play() {
+		this.gameLoop = true;
+		do {
+			this._drawPiece();
+			this._process();
+			await this._sleep();
+		} while(this.gameLoop);
 	}
 	
-	nextShape() {
-		this.pieceShape = (1 + this.pieceShape) % 4;	
+	_process() {
+		++this.frameCounter;
 	}
-
-	update() {
-		this.potentialPos = this.pos;	
-
-		this.fillPiece();
-	}
-}
-
-class JTetris {
-	pressedKeys = new Set();
-	key;
-	constructor(context) {
-		this.context = context;
-		this.board = new Board(this.context);
-		this.piece = new Piece(new V2(0, 0), this.context);
-		this.moved = false;
-	}
-
-	update() {
-		let vel = new V2(0, 0);
-
-		if (this.key in directionMap && !this.moved) {
-			vel = vel.add(directionMap[this.key]);
-			this.moved = true;
-		} 
-
- 		if (this.piece.canSetPos(new V2(this.piece.potentialPos.x, this.piece.potentialPos.y + 20))) {
-			this.piece.potentialPos.y += 20;
-			this.piece.pos = this.piece.potentialPos;
-		} else if (this.piece.potentialPos.y <= this.piece.pos.y) {
-			this.piece.freeze();
-			this.piece.new();
-			this.board.update();
-		}
-
-		this.piece.potentialPos = this.piece.potentialPos.add(vel.scale(20));	
-		this.moved = false;
-
-		if (this.piece.potentialPos.x < 220 - this.piece.getWidth() &&
-				this.piece.potentialPos.x >= 0 && this.piece.potentialPos.y < 420 - this.piece.getHeight()) {
-			if (this.piece.canSetPos(this.piece.potentialPos)) {
-				this.piece.pos = this.piece.potentialPos;
-				console.log(this.piece.canSetPos(this.piece.potentialPos))
+	
+	_drawPiece() {
+		let piece = this.piece.rotation[this.pieceRotation];
+		for (let i = 0; i < piece.length; i++) {
+			for (let j = 0; j < piece[i].length; j++) {
+				if (piece[i][j] != 0) {
+					this._drawSquare(
+						this.piecePosition[0] + j * this.squareSide,
+						this.piecePosition[1] + i * this.squareSide, 
+						this.piece.color);
+				}
 			}
-		} else {
-			console.log("FALSE")
-			console.log("Piece WIDTH = " + this.piece.getWidth())
 		}
-
-		this.board.update();
-		this.piece.update();
-		console.log(this.piece.pos.y)
 	}
 
-	keyDown(event) {
-		this.pressedKeys.add(event.code);
-		this.key = event.code;
+	_drawSquare(x, y, color) {
+		this.context.fillStyle = color;
+		this.context.fillRect(x, y, this.squareSide, this.squareSide);
 	}
 
-	keyUp(event) {
-		this.pressedKeys.delete(event.code);
-		this.key = "";
-	}
+	_sleep() {return new Promise(requestAnimationFrame); }
+	
 }
-
-let game = null;
 
 (() => {
 	const canvas = document.getElementById('game-canvas');
 	const context = canvas.getContext('2d');
-	canvas.width = 1000;
-	canvas.height = 400;
+	const game = new Tetris(context);
 
-	let game = new JTetris(context);
-	let start = 0;
+	game.play();
+}) ()
 
-	function step(timestamp) {
-		if (start === undefined)  {
-			start = timestamp;
-		}
-		
-		if (!start || timestamp - start >= 100) {
-			start = timestamp;
-			game.update();
-		}
-		window.requestAnimationFrame(step);
-	}
-
-	window.requestAnimationFrame(step);
-
-	document.addEventListener('keydown', event => {
-		game.keyDown(event);
-	})
-
-	document.addEventListener('keyup', event => {
-		game.keyUp(event);
-	})
-})()
